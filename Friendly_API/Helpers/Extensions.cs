2ecefec6b66ddda41fd164ce6da412ae7c096b96
Martin Szauer <mszauer@Martins-MacBook-Pro.local>
2018-03-26 17:52:17 -0400

    fix error handling

--- Friendly_API/Helpers/Extensions.cs
+++ Friendly_API/Helpers/Extensions.cs
@@ -6,8 +6,8 @@ namespace Friendly.Helpers
     {
         public static void AddApplicationError(this HttpResponse response, string message)
         {
-            response.Headers.Add("Application Error", message);
-            response.Headers.Add("Access-Control-Expose-Headers", "Application Error");
+            response.Headers.Add("Application-Error", message);
+            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
     }
